e39681a82ab502f0196e37725fe471017881bc09
qukd <qukd@ifeng.com>
2016-10-25 16:25:40 +0800

    keda update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+
 var request = require('request');
 
 var pwd = process.env.PWD;
@@ -28,6 +29,7 @@ fs.readdir(tmp, function(err, data) {
                     console.log('返回值：' + body)
                     var date = new Date();
                     var log = date + '  ' + body + '\n';
+                    
                     fs.appendFile('log.txt', log, function(err) {
                         if (err) throw err;
                     })
@@ -41,57 +43,3 @@ fs.readdir(tmp, function(err, data) {
 
 
 
-
-// fs.readFile(pwd + '/tmp/123.jpg', function(err, data) {
-//     if (err) {
-//         console.log('err:' + err);
-//     } else {
-//         //var text = data.toString('utf-8');
-//         // console.log(text);
-//         //var buf = new Buffer(text, 'utf-8')
-//         // console.log(buf);
-//         //console.log(data);
-//         //console.log(data.length + ' bytes');
-//     }
-// });
-
-
-
-
-// var data = '{"name":123}';
-
-// fs.writeFile('output.txt', data, function (err) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         console.log('ok.');
-//     }
-// });
-// fs.readFile('output.txt', 'utf-8', function(err, data) {
-//     if (err) {
-//         console.log('err:' + err);
-//     } else {
-//      var json = JSON.parse(data);
-//         console.log(json.name);
-//     }
-// });
-
-
-// fs.stat('output.txt', function (err, stat) {
-//     if (err) {
-//         console.log(err);
-//     } else {
-//         // 是否是文件:
-//         console.log('isFile: ' + stat.isFile());
-//         // 是否是目录:
-//         console.log('isDirectory: ' + stat.isDirectory());
-//         if (stat.isFile()) {
-//             // 文件大小:
-//             console.log('size: ' + stat.size);
-//             // 创建时间, Date对象:
-//             console.log('birth time: ' + stat.birthtime);
-//             // 修改时间, Date对象:
-//             console.log('modified time: ' + stat.mtime);
-//         }
-//     }
-// });
